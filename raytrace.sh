5aa8b4a16fd052e0980ae0929bb6b05801119fc8
Jan Scholz <scholz_sfo+github@hotmail.com>
2015-06-29 11:38:52 -0400

    remove superflous comments from raytrace.sh

--- raytrace.sh
+++ raytrace.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-# create slice images for enriched b6 publication
+# create slice images
 # 2014-07-03,  jan.scholz@mouseimaging.ca
 #
 # TODO:
@@ -153,10 +153,6 @@ rm -rf $TDIR
 exit 0
 
 
-
-
-# cd /projects/mice/jscholz/rot/stats_hr_new2+flip/dti_to_basket
-
 # Website with all the information:
 # 
 # http://www.bic.mni.mcgill.ca/~david/Ray_trace/ray_trace_tutorial.html
@@ -168,34 +164,3 @@ exit 0
 # 180  0 0 0
 # 190 0 0 1
 # 255 1 1 1
-
-
-
-# create cluster-extend thresholded signed images
-# minccalc -expression 'A[0]>56?A[1]:0' clusters_dti_meanCmeanFA_lmerp_mean-p-sign_to_basket_thr0.005.mnc dti_meanCmeanFA_lmerp_mean-p-sign_to_basket.mnc clusters_dti_meanCmeanFA_lmerp_mean-p-sign_to_basket_thr0.005_gt56.mnc
-# minccalc -expression 'A[0]>56?A[1]:0' clusters_dti_faCmeanFA_lmerp_grouprotarod-p-sign_to_basket_thr0.005.mnc  dti_faCmeanFA_lmerp_grouprotarod-p-sign_to_basket.mnc  clusters_dti_faCmeanFA_lmerp_grouprotarod-p-sign_to_basket_thr0.005_gt56.mnc
-# minccalc -expression 'A[0]>56?A[1]:0' clusters_final_groupPsexRsideRid_lmerp_grouprotarod-p-corr-sign_thr0.1.mnc  final_groupPsexRsideRid_lmerp_grouprotarod-p-corr-sign.mnc clusters_final_groupPsexRsideRid_lmerp_grouprotarod-p-corr-sign_thr0.1_gt56.mnc
-# minccalc -expression 'A[0]>56?A[1]:0' clusters_final_meanPsexRsideRid_lmerp_mean-p-corr-sign_thr0.1.mnc  final_meanPsexRsideRid_lmerp_mean-p-corr-sign.mnc clusters_final_meanPsexRsideRid_lmerp_mean-p-corr-sign_thr0.1_gt56.mnc 
-
-
-#awk 'NR>1&&NR<5 {print $4}' clusters_dti_faCmeanFA_lmerp_grouprotarod-p-sign_to_basket_thr0.005.mm.txt | ./raytrace.sh -v -b template_basket_masked.mnc -l resampled_atlas_new_stead+ull_right.mnc -s blender_constrained_smooth.obj -i clusters_dti_faCmeanFA_lmerp_grouprotarod-p-sign_to_basket_thr0.005_gt56.mnc -t 0.995 -o slices/dti_group
-
-#awk 'NR>1&&NR<44 {print $4}' clusters_dti_meanCmeanFA_lmerp_mean-p-sign_to_basket_thr0.005.mm.txt | ./raytrace.sh -v -b template_basket_masked.mnc -l resampled_atlas_new_stead+ull_right.mnc -s blender_constrained_smooth.obj -i clusters_dti_meanCmeanFA_lmerp_mean-p-sign_to_basket_thr0.005_gt56.mnc -t 0.995 -o slices/dti_behav_mean
-
-#awk 'NR>1&&NR<11 {print $4}' clusters_final_meanPsexRsideRid_lmerp_mean-p-corr-sign_thr0.1.mm.txt | ./raytrace.sh -v -b template_basket_masked.mnc -l resampled_atlas_new_stead+ull_right.mnc -s blender_constrained_smooth.obj -o slices/vol_beahv_mean clusters_final_meanPsexRsideRid_lmerp_mean-p-corr-sign_thr0.1_gt56.mnc 0.9
-######awk 'NR>1&&NR<11 {print $4}' clusters_final_meanPsexRsideRid_lmerp_mean-p-corr-sign_thr0.1.mm.txt | ./raytrace.sh -v -b template_basket_masked.mnc -l resampled_atlas_new_stead+ull_right.mnc -s blender_constrained_smooth.obj -i clusters_final_meanPsexRsideRid_lmerp_mean-p-corr-sign_thr0.1_gt56.mnc -t 0.9 -o slices/vol_beahv_mean
-
-#awk 'NR>1&&NR<21 {print $4}' clusters_final_groupPsexRsideRid_lmerp_grouprotarod-p-corr-sign_thr0.1.mm.txt | ./raytrace.sh -v -b template_basket_masked.mnc -l resampled_atlas_new_stead+ull_right.mnc -s blender_constrained_smooth.obj -o slices/vol_group clusters_final_groupPsexRsideRid_lmerp_grouprotarod-p-corr-sign_thr0.1_gt56.mnc 0.9
-######awk 'NR>1&&NR<21 {print $4}' clusters_final_groupPsexRsideRid_lmerp_grouprotarod-p-corr-sign_thr0.1.mm.txt | ./raytrace.sh -v -b template_basket_masked.mnc -l resampled_atlas_new_stead+ull_right.mnc -s blender_constrained_smooth.obj -i clusters_final_groupPsexRsideRid_lmerp_grouprotarod-p-corr-sign_thr0.1_gt56.mnc -t 0.9 -o slices/vol_group
-
-# paste this into svg file:
-# i=0; for f in `ls slices-src/dti_behav_mean_y*png | sort -r`; do x=$(($i%3*210-500)); y=$(($i/3*150)); echo "<image xlink:href=\"$f\" x=\"$x\" y=\"$y\" height=\"170.8\" width=\"223.3\" />"; let i+=1;  done`
-
-# 3.40
-# z = 3.14
-
-
-# hippocampus hotspot of changes
-# printf " -0.63\n-0.69\n" | ./raytrace.sh -v -b template_basket_masked.mnc -l resampled_atlas_new_stead+ull_right.mnc -s blender_constrained_smooth.obj -o slices/hotspot clusters_final_groupPsexRsideRid_lmerp_grouprotarod-p-corr-sign_thr0.1_gt56.mnc 0.9 clusters_dti_faCmeanFA_lmerp_grouprotarod-p-sign_to_basket_thr0.005_gt56.mnc 0.995 clusters_dti_meanCmeanFA_lmerp_mean-p-sign_to_basket_thr0.005_gt56.mnc 0.995
-
-
